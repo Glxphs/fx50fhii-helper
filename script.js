51d2aabb586bf4841e3f16137d0984c078acb711
Glxphs <76955207+Glxphs@users.noreply.github.com>
2022-12-07 00:09:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,22 +21,19 @@ for (let key in symbolsObj) {
 }
 
 function append(myValue) {
-    //IE support
-    myValue = myValue.innerHTML;
     const myField = document.getElementById("code")
     if (document.selection) {
         myField.focus();
         sel = document.selection.createRange();
-        sel.text = myValue;
+        sel.text = myValue.innerHTML;
     }
-    //MOZILLA and others
     else if (myField.selectionStart || myField.selectionStart == '0') {
         var startPos = myField.selectionStart;
         var endPos = myField.selectionEnd;
         myField.value = myField.value.substring(0, startPos)
-            + myValue
+            + myValue.innerHTML
             + myField.value.substring(endPos, myField.value.length);
     } else {
-        myField.value += myValue;
+        myField.value += myValue.innerHTML;
     }
 }
